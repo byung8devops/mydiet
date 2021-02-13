047a1b67e598238a3ac03b5f0c6949e81144e3f3
byung8lim <byung8lim@gmail.com>
2021-02-14 08:47:06 +0900

    2/14 1

--- src/main/java/com/byung8/mydiet/controller/FitnessController.java
+++ src/main/java/com/byung8/mydiet/controller/FitnessController.java
@@ -17,9 +17,7 @@ import com.byung8.common.domain.Result;
 import com.byung8.common.domain.SearchOption;
 import com.byung8.common.exception.Byung8Exception;
 import com.byung8.mydiet.domain.ExerciseParam;
-import com.byung8.mydiet.domain.FitnessMember;
 import com.byung8.mydiet.service.ExerciseService;
-import com.byung8.mydiet.service.FitnessMemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,122 +29,14 @@ import lombok.extern.slf4j.Slf4j;
  */
 @RestController
 @Slf4j
-@RequestMapping(value = "/fitness")
+@RequestMapping(value = "/diet")
 public class FitnessController {
 
 	@Autowired
 	ExerciseService exerciseService;
 	
-	@Autowired
-	FitnessMemberService fitnessMemberService;
-	
 	private String txId() {
 		return UUID.randomUUID().toString();
-	}
-
-	@RequestMapping(value = "/member", method = RequestMethod.POST)
-	public ResponseEntity<String> registerExerciseParam(@RequestBody final FitnessMember fitnessMember) {
-
-		String txid = txId();
-		try {
-			Result result = fitnessMemberService.registerFsMember(fitnessMember, txid);
-			return new ResponseEntity<String>(result.toJson(), result.status());
-		} catch (Exception e) {
-			log.error("Failed to register FitnessMember", e);
-			if (e instanceof NullPointerException) {
-				return new ResponseEntity<String>(HttpStatus.EXPECTATION_FAILED);
-			} else if (e instanceof Byung8Exception) {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e.getMessage());
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			} else {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e);
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			}
-		}
-	}
-
-	@RequestMapping(value = "/member/{email}/email", method = RequestMethod.GET)
-	public ResponseEntity<String> findMemberByEmail(@PathVariable("email") final String email) {
-
-		String txid = txId();
-		try {
-			Result result = fitnessMemberService.findFsMemberByEmail(email, txid);
-			return new ResponseEntity<String>(result.toJson(), result.status());
-		} catch (Exception e) {
-			log.error("Failed to findFsMemberByEmail", e);
-			if (e instanceof NullPointerException) {
-				return new ResponseEntity<String>(HttpStatus.EXPECTATION_FAILED);
-			} else if (e instanceof Byung8Exception) {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e.getMessage());
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			} else {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e);
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			}
-		}
-	}
-
-	@RequestMapping(value = "/member/{name}/name", method = RequestMethod.GET)
-	public ResponseEntity<String> findMemberByName(@PathVariable("name") final String name) {
-
-		String txid = txId();
-		try {
-			Result result = fitnessMemberService.findFsMemberByName(name, txid);
-			return new ResponseEntity<String>(result.toJson(), result.status());
-		} catch (Exception e) {
-			log.error("Failed to findFsMemberByName", e);
-			if (e instanceof NullPointerException) {
-				return new ResponseEntity<String>(HttpStatus.EXPECTATION_FAILED);
-			} else if (e instanceof Byung8Exception) {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e.getMessage());
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			} else {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e);
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			}
-		}
-	}
-
-	@RequestMapping(value = "/member/{id}/id", method = RequestMethod.GET)
-	public ResponseEntity<String> findMemberById(@PathVariable("id") final int id) {
-
-		String txid = txId();
-		try {
-			Result result = fitnessMemberService.findFsMemberById(id, txid);
-			return new ResponseEntity<String>(result.toJson(), result.status());
-		} catch (Exception e) {
-			log.error("Failed to findFsMemberById", e);
-			if (e instanceof NullPointerException) {
-				return new ResponseEntity<String>(HttpStatus.EXPECTATION_FAILED);
-			} else if (e instanceof Byung8Exception) {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e.getMessage());
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			} else {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e);
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			}
-		}
-	}
-
-	@RequestMapping(value = "/member/list", method = RequestMethod.POST)
-	public ResponseEntity<String> findMemberList(@RequestBody final SearchOption searchOption) {
-
-		String txid = txId();
-		try {
-			Result result = fitnessMemberService.findFsMemberList(searchOption, txid);
-			return new ResponseEntity<String>(result.toJson(), result.status());
-		} catch (Exception e) {
-			log.error("Failed to findFsMemberList", e);
-			if (e instanceof NullPointerException) {
-				return new ResponseEntity<String>(HttpStatus.EXPECTATION_FAILED);
-			} else if (e instanceof Byung8Exception) {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e.getMessage());
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			} else {
-				Result result = new Result(txid, IResult.ERROR, "").putValue("error", e);
-				return new ResponseEntity<String>(result.toJson(), HttpStatus.EXPECTATION_FAILED);
-			}
-		}
 	}
 
 	@RequestMapping(value = "/exercise/calorie/{paramId}/{weight}/{minute}", method = RequestMethod.GET)
